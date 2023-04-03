09a872e63efbd87730d1b97a72127f4bd2872bbe
Dani <d.tarazoma@geekshubs.com>
2023-04-03 11:54:44 +0200

    feat: migration pizzas entity

---
+++ database/migrations/2023_04_03_085712_create_pizzas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->boolean('is_active')->default(true);
+            $table->enum("type", ['fina', 'pan_pizza','original']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pizzas');
+    }
+};

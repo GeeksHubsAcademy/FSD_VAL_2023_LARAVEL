685a5d01026e22f7e283c859a88cd6d38d20194d
Dani <d.tarazoma@geekshubs.com>
2023-04-06 10:16:19 +0200

    feat: roles and foreing role_id to users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
         $this->call([
+            RoleSeeder::class,
             PizzaSeeder::class,
         ]);
-
-        \App\Models\Review::factory(15001)->create();
+        
+        \App\Models\User::factory(10)->create();
+        \App\Models\Review::factory(150)->create();
     }
 }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert(
+            [
+                [
+                    'id' => 1,
+                    'name' => "user"
+                ],
+                [
+                    'id' => 2,
+                    'name' => "admin"
+                ],
+                [
+                    'id' => 3,
+                    'name' => "super_admin"
+                ]
+            ]
+        );
+    }
+}
